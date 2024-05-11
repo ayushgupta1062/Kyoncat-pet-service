509c8fbe8d3eb0eac56db3be99ea8b42bde0e0e9
jayakrishnan <jayakrishnanr@carotecs.com>
2024-05-11 15:19:37 +0530

    removed services

--- kyonkat/settings.py
+++ kyonkat/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-q+)z$cqsz5(88fey%kz0efs-m2+)s#+3q-s5%nu(dqnhs@zy8x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
